e949892a3b839f7478660194d363225a6b3f566a
Yaniv Raz <yanivmiz77@gmail.com>
2026-06-23 15:47:21 +0300

    test(site): capture button smoke diagnostics

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,14 @@ module.exports = {
     headless: true,
     viewport: { width: 1440, height: 1000 },
     ignoreHTTPSErrors: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure',
   },
+  reporter: [
+    ['line'],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+  ],
   projects: [
     {
       name: 'chromium',
